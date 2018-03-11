29aabff667a7790487c59f04b51662eb3e40f8de
Adetuyi Tolu Emmanuel <tolu.adetuyi@gmail.com>
2018-03-12 00:37:54 +0100

    Update LanguageUnitTest.java

--- nglocaltimes/src/test/java/com/crevation/nglocaltime/LanguageUnitTest.java
+++ nglocaltimes/src/test/java/com/crevation/nglocaltime/LanguageUnitTest.java
@@ -13,9 +13,8 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 
 /**
- * Example local unit test, which will execute on the development machine (host).
+ * LanguageTime library unit test, which will execute on the development machine (host).
  *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class LanguageUnitTest {
 
